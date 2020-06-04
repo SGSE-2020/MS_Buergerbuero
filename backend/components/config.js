3704279a3868418e3b17844fc635c3ce56c42897
Pia Schreiner <pia.schreiner@outlook.com>
2020-06-04 21:05:03 +0200

    Fix Dockerfile

--- backend/components/config.js
+++ backend/components/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-    //DB_HOST: "db",
-    DB_HOST: "localhost",
+    DB_HOST: "db",
+    //DB_HOST: "localhost",
     DB_USER: "buergerbuero",
     DB_PASSWORD: "sgse-ss2020",
     DB_NAME: "buergerbuero",
     DB_DIALECT: "postgres",
 
-    //BACKEND_HOST: "backend",
-    BACKEND_HOST: "localhost",
+    BACKEND_HOST: "backend",
+    //BACKEND_HOST: "localhost",
 
     MESSAGE_QUEUE: 'queue-test'
 };
